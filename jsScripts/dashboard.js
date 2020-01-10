270bf63501dbea4aae66fd31d6dbf7a3174109b7
Rohit-dev-coder <jarariyarohti171198@gmail.com>
2020-01-10 16:24:10 +0530

    Half Dashboard Complete

--- jsScripts/dashboard.js
+++ jsScripts/dashboard.js
@@ -1,6 +1,26 @@
 // default
 
+window.onresize = function() {
+       this.onloadmyfunction();
+};
+window.onload = function() {
+    this.onloadmyfunction();
+};
 
+function onloadmyfunction()
+{
+    var x = window.innerWidth;
+    let Links = document.querySelector(".nav");
+    if (x<768)
+    {        
+        let navbar = document.querySelector(".navbar-nav");
+        navbar.append(Links);
+    }
+    else{
+        let side = document.querySelector(".sidebar-sticky");
+        side.append(Links);
+    }
+}
 
 
 
