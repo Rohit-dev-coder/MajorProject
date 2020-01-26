da422bdb4e9d9b4cbeaaddc396ae97eccff8fb14
veer11997 <virendra11997@gmail.com>
2020-01-26 21:39:58 +0530

    new commit

---
+++ src/java/techquiz/controller/LoginControllerServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package techquiz.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import techquiz.dao.UserDAO;
+import techquiz.dto.UserDTO;
+
+/**
+ *
+ * @author hp
+ */
+public class LoginControllerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         RequestDispatcher rd=null;
+            String username=request.getParameter("username");
+            String password=request.getParameter("password");
+            UserDTO user=new UserDTO(username,password);
+            try{
+                String result=UserDAO.validateUser(user);
+                System.out.println(result);
+                request.setAttribute("result",result);
+                request.setAttribute("username",username);
+                rd=request.getRequestDispatcher("loginresponse.jsp");
+               }
+            catch(SQLException e) {
+            request.setAttribute("exception",e);
+            rd=request.getRequestDispatcher("showexception.jsp");
+            e.printStackTrace();
+                 }
+        finally
+        {
+            rd.forward(request, response);
+        }
+     
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/loginresponse.jsp
@@ -0,0 +1,35 @@
+<%-- 
+    Document   : loginresponse
+    Created on : 26 Jan, 2020, 9:18:11 PM
+    Author     : hp
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    
+    String username=(String)request.getAttribute("username");
+      String result=(String)request.getAttribute("result");
+    
+    if(username!=null&&result!=null)
+    {
+        HttpSession sess=request.getSession();
+        sess.setAttribute("username",username);
+        if(result.equalsIgnoreCase("student"))
+        {
+         String url="studentdashboard.html;jsessionid="+session.getId();
+        out.println(url);
+        
+         }
+    else if(result.equalsIgnoreCase("teacher"))  
+    {
+      String url="teacherdashboard.html;jsessionid="+session.getId();
+      out.println(url);
+        
+    }
+    }
+    else
+    {
+        out.println("error");
+    }
+    %>
+    
